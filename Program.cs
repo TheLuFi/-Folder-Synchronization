b1ab0943dc88cbd425be6b31c2b818f8824e9dec
TheLuFi <150939546+TheLuFi@users.noreply.github.com>
2023-11-26 18:44:43 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -131,7 +131,6 @@ static void Log(string logMessage, TextWriter w)
 {
     w.Write("\r\nLog Entry : ");
     w.WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
-    w.WriteLine("  :");
     w.WriteLine($"  :{logMessage}");
     w.WriteLine("-------------------------------");
 }
